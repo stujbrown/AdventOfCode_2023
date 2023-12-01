ca7c631cb99d1c5982ae9f4d12f27fe88eb42b88
Stuart Brown <stuart.j.brown.90@gmail.com>
2023-12-01 14:25:47 +0000

    More concise day1

--- src/days/day1.cpp
+++ src/days/day1.cpp
@@ -6,30 +6,21 @@ int add_line(const std::string& line, bool allow_alpha)
     static const std::vector<std::string> digits_numeric_alpha { "1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
     const std::vector<std::string>& digits = allow_alpha ? digits_numeric_alpha : digits_numeric;
     
-    size_t first_digit = 0, last_digit = 0;
-    size_t best_found = std::string::npos;
-    for (size_t i = 0; i < digits.size(); ++i)
+    int first_digit = -1, last_digit = -1;
+    for (size_t i = 0; i < line.size(); ++i)
     {
-        const size_t found = line.find(digits[i]);
-        if (found != std::string::npos && found < best_found)
+        for (size_t digit_index = 0; digit_index < digits.size(); ++digit_index)
         {
-            first_digit = i;
-            best_found = found;
+            const int digit = digit_index % 9;
+            if (strncmp(line.c_str() + i, digits[digit_index].c_str(), digits[digit_index].length()) == 0)
+            {
+                if (first_digit == -1)
+                    first_digit = digit;
+                last_digit = digit;
+            }
         }
     }
-    
-    last_digit = first_digit;
-    best_found = 0;
-    for (size_t i = 0; i < digits.size(); ++i)
-    {
-        const size_t found = line.rfind(digits[i]);
-        if (found != std::string::npos && found > best_found)
-        {
-            last_digit = i;
-            best_found = found;
-        }
-    }
-    
+
     return (((first_digit % 9) + 1) * 10) + (last_digit % 9) + 1;
 }
 
