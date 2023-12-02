6a9fa8a20b818a7fe95951cda533b2b726e64bb5
Stuart Brown <stuart.j.brown.90@gmail.com>
2023-12-02 17:19:30 +0000

    All days added

--- src/days/aoc_days.hpp
+++ src/days/aoc_days.hpp
@@ -8,5 +8,28 @@ namespace aoc
 
 void day1();
 void day2();
+inline void day3() {}
+inline void day4() {}
+inline void day5() {}
+inline void day6() {}
+inline void day7() {}
+inline void day8() {}
+inline void day9() {}
+inline void day10() {}
+inline void day11() {}
+inline void day12() {}
+inline void day13() {}
+inline void day14() {}
+inline void day15() {}
+inline void day16() {}
+inline void day17() {}
+inline void day18() {}
+inline void day19() {}
+inline void day20() {}
+inline void day21() {}
+inline void day22() {}
+inline void day23() {}
+inline void day24() {}
+inline void day25() {}
 
 }
--- src/entry.cpp
+++ src/entry.cpp
@@ -27,10 +27,34 @@ void entry(const Args& args)
         std::cout << std::endl;
     }
     
+    #define DAY(number) case number: day##number(); break;
     switch (day_to_run)
     {
-        case 1: day1(); break;
-        case 2: day2(); break;
+        DAY(1);
+        DAY(2);
+        DAY(3);
+        DAY(4);
+        DAY(5);
+        DAY(6);
+        DAY(7);
+        DAY(8);
+        DAY(9);
+        DAY(10);
+        DAY(11);
+        DAY(12);
+        DAY(13);
+        DAY(14);
+        DAY(15);
+        DAY(16);
+        DAY(17);
+        DAY(18);
+        DAY(19);
+        DAY(20);
+        DAY(21);
+        DAY(22);
+        DAY(23);
+        DAY(24);
+        DAY(25);
             
         default:
             std::cout << "Invalid day\n";
