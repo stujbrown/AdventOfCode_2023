b045627ee8e8fac77f30c03bef2d57d0e4f974ae
Stuart Brown <stuart.j.brown.90@gmail.com>
2023-12-02 17:04:54 +0000

    Day 2 finished

--- src/days/day2.cpp
+++ src/days/day2.cpp
@@ -6,59 +6,50 @@
 
 void aoc::day2()
 {
-    int possible_game_sum = 0;
-
-    const std::array<std::string_view, 3> colours { "red", "green", "blue" };
-    std::vector<int> possible_games;
-
     std::ifstream file("inputs/day2.txt");
     std::string line;
+
+    const std::array<std::string_view, 3> colours{ "red", "green", "blue" };
+    int possible_game_sum = 0;
+    int power_sum = 0;
+
     while (std::getline(file, line, '\n'))
     {
-        std::array<size_t, 3> max_counts{ 0, 0, 0 };
+        std::array<int, 3> max_counts{ 0, 0, 0 };
 
         const size_t separator_idx = line.find_first_of(':');
-        const std::string_view id(line.c_str() + 5, separator_idx - 5);
+        const std::string id(line.c_str() + 5, separator_idx - 5);
 
         size_t value_start = separator_idx + 1;
         for (size_t i = separator_idx; i < line.length(); ++i)
         {
-            if (line[i] == ',')
+            if (line[i] == ',' || line[i] == ';')
             {
                 value_start = i + 1;
+                continue;
             }
-            else if (line[i] == ';')
-            {
-                value_start = i + 1;
-                // Rounds are currently irrelevant
-            }
-            else
-            {
-                for (size_t colour_idx = 0; colour_idx < colours.size(); ++colour_idx)
-                {
-                    if (strncmp(line.c_str() + i, colours[colour_idx].data(), colours[colour_idx].length()) == 0)
-                    {
-                        std::string num_str(line.c_str() + value_start, i - value_start);
-                        num_str.erase(std::remove_if(num_str.begin(), num_str.end(), std::isspace), num_str.end());
-                        const int value = std::stoi(num_str);
-                        max_counts[colour_idx] = std::max(max_counts[colour_idx], (size_t)value);
 
-                        i += colours[colour_idx].length() - 1;
-                        break;
-                    }
+            for (size_t colour_idx = 0; colour_idx < colours.size(); ++colour_idx)
+            {
+                if (strncmp(line.c_str() + i, colours[colour_idx].data(), colours[colour_idx].length()) == 0)
+                {
+                    std::string num_str(line.c_str() + value_start, i - value_start);
+                    num_str.erase(std::remove_if(num_str.begin(), num_str.end(), std::isspace), num_str.end());
+                    const int value = std::stoi(num_str);
+                    max_counts[colour_idx] = std::max(max_counts[colour_idx], value);
+
+                    i += colours[colour_idx].length() - 1;
+                    break;
                 }
             }
         }
 
         if (max_counts[0] <= 12 && max_counts[1] <= 13 && max_counts[2] <= 14)
-        {
-            int game_id = 0;
-            std::from_chars(id.data(), id.data() + id.length(), game_id);
-            possible_games.push_back(game_id);
+            possible_game_sum += std::stoi(id);
 
-            possible_game_sum += game_id;
-        }
+        power_sum += max_counts[0] * max_counts[1] * max_counts[2];
     }
 
     std::cout << "Sum of possible games: " << possible_game_sum << std::endl;
+    std::cout << "Sum of powers: " << power_sum << std::endl;
 }
