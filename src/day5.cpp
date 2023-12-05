49c855333ce431ca25d1a302cac26340ecb5b01f
stujbrown <75371400+stujbrown@users.noreply.github.com>
2023-12-05 10:29:28 +0000

    Create day5.cpp

---
+++ src/day5.cpp
@@ -0,0 +1,17 @@
+#include "aoc_days.hpp"
+#include <>
+
+void aoc::day5()
+{
+  std::ifstream file("inputs/day5.txt");
+  std::string line;
+  std::getline(file, line);
+
+  //std::stringstream ss(line.c_str() + 6);
+  //line.c_str() + 6
+
+    while (file >> line)
+    {
+        line
+    }
+}
