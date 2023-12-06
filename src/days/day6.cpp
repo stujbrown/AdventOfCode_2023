a875a4e9bda19b0de78ca5eb5ed7e1f8737136a9
Stuart Brown <stuart.j.brown.90@gmail.com>
2023-12-06 14:14:19 +0000

    Cleanup

--- src/days/day6.cpp
+++ src/days/day6.cpp
@@ -2,28 +2,16 @@
 #include <vector>
 #include <cctype>
 
-long solve(const std::vector<long>& durations, const std::vector<long>& times)
+long solve_race(long duration, long win_record)
 {
-    long ways_to_win_multiplied = 0;
-    for (size_t game_idx = 0; game_idx < durations.size(); ++game_idx)
+    long ways_to_win = 0;
+    for (long held_ms = 1; held_ms < duration; ++held_ms)
     {
-        long ways_to_win = 0;
-        for (long held_ms = 1; held_ms < durations[game_idx]; ++held_ms)
-        {
-            const long distance = held_ms * (durations[game_idx] - held_ms);
-            if (distance > times[game_idx])
-            {
-                ++ways_to_win;
-            }
-        }
-        
-        if (ways_to_win_multiplied == 0)
-            ways_to_win_multiplied = ways_to_win;
-        else
-            ways_to_win_multiplied *= ways_to_win > 0 ? ways_to_win : 1;
+        const long distance = held_ms * (duration - held_ms);
+        if (distance > win_record)
+            ++ways_to_win;
     }
-    
-    return ways_to_win_multiplied;
+    return ways_to_win;
 }
 
 void aoc::day6()
@@ -31,7 +19,7 @@ void aoc::day6()
     std::ifstream file("inputs/day6.txt");
     std::string token;
     int line_idx = -1;
-    std::vector<long> durations, times, combimed_duration, combined_time;
+    std::vector<long> durations, times;
     std::string big_duration_str, big_time_str;
     while (file >> token)
     {
@@ -43,11 +31,18 @@ void aoc::day6()
             (line_idx == 0 ? big_duration_str : big_time_str) += token;
         }
     }
-    combimed_duration.push_back(std::stol(big_duration_str));
-    combined_time.push_back(std::stol(big_time_str));
     
-    const long ways_to_win_multiplied = solve(durations, times);
-    const long ways_to_win_big_race = solve(combimed_duration, combined_time);
+    long ways_to_win_multiplied = 0;
+    for (size_t game_idx = 0; game_idx < durations.size(); ++game_idx)
+    {
+        const long ways_to_win = solve_race(durations[game_idx], times[game_idx]);
+        if (ways_to_win_multiplied == 0)
+            ways_to_win_multiplied = ways_to_win;
+        else
+            ways_to_win_multiplied *= ways_to_win > 0 ? ways_to_win : 1;
+    }
+    
+    const long ways_to_win_big_race = solve_race(std::stol(big_duration_str), std::stol(big_time_str));
     
     std::cout << "Ways to win multiplied: " << ways_to_win_multiplied << std::endl;
     std::cout << "Ways to win the big race: " << ways_to_win_big_race << std::endl;
