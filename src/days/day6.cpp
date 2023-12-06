a0c67806a5eed78cd02845dc015ad575cb1a6477
stujbrown <75371400+stujbrown@users.noreply.github.com>
2023-12-06 13:27:05 +0000

    Create day6.cpp

---
+++ src/days/day6.cpp
@@ -0,0 +1,13 @@
+
+void aoc::day6()
+{
+    std::ifstream file("inputs/day6.txt");
+    std::string token;
+    int line_idx = 0;
+    std::vector<int> durations, times;
+    while (file >> line)
+    {
+        if (std::isalpha(line))
+    }
+    
+}
