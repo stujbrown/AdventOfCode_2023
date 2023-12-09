18a74dda6138ecc070dddb14be4c6ebaf69d7dc9
stujbrown <75371400+stujbrown@users.noreply.github.com>
2023-12-09 17:43:13 +0000

    Create day9.cpp

---
+++ src/days/day9.cpp
@@ -0,0 +1,19 @@
+#include "aoc_days.hpp"
+
+void aoc::day9()
+{
+    std::ifstream file("inputs/day9.txt");
+    std::string line;
+    while (std::getline(file, line))
+    {
+        size_t start = 0, end = 0;
+        while (end != std::string::npos)
+        {
+            end = line.find(" ", start);
+            if (end != std::string::npos)
+            {
+                
+            }
+        }
+    }
+{
