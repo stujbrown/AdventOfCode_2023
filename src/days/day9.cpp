d1bbbfed9c504f28e573975bccebed89c97ec284
stujbrown <75371400+stujbrown@users.noreply.github.com>
2023-12-09 18:17:47 +0000

    Parsing

--- src/days/day9.cpp
+++ src/days/day9.cpp
@@ -6,14 +6,19 @@ void aoc::day9()
     std::string line;
     while (std::getline(file, line))
     {
+        std::vector<std::vector<int>> sequences;
         size_t start = 0, end = 0;
         while (end != std::string::npos)
         {
             end = line.find(" ", start);
             if (end != std::string::npos)
             {
-                
+                std::string value_str(line.begin() + start, end - start);
+                const int value = std::stoi(value_str);
+                sequence.push_back(value);
             }
         }
+
+        
     }
 {
