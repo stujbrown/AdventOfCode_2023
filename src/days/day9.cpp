94c13e53fc53de3cb9e8905fdc637c172d5e58af
Stuart Brown <stuart.j.brown.90@gmail.com>
2023-12-09 23:04:50 +0000

    Day 9 part 1

--- src/days/day9.cpp
+++ src/days/day9.cpp
@@ -1,7 +1,10 @@
 #include "aoc_days.hpp"
+#include <vector>
 
 void aoc::day9()
 {
+    int end_total = 0;
+    
     std::ifstream file("inputs/day9.txt");
     std::string line;
     while (std::getline(file, line))
@@ -11,33 +14,50 @@ void aoc::day9()
         while (end != std::string::npos)
         {
             end = line.find(" ", start);
-            if (end != std::string::npos)
-            {
-                std::string value_str(line.begin() + start, end - start);
-                const int value = std::stoi(value_str);
-                sequences[0].push_back(value);
-            }
+            std::string value_str;
+            if (end == std::string::npos)
+                value_str = std::string(line.c_str() + start);
+            else
+                value_str = std::string(line.c_str() + start, end - start);
+            
+            const int value = std::stoi(value_str);
+            sequences[0].push_back(value);
+            start = end + 1;
         }
-
-        size_t sequence_idx = 0;
+        
+        int sequence_idx = 0;
         bool all_zero = false;
-
-        
-        
         
         while (!all_zero)
         {
+            all_zero = true;
             sequences.push_back(std::vector<int>());
-            int previous = sequences[sequence_idx];
-            for (size_t i = 0; i < sequences[sequence_idx].size(); ++i)
+            int previous = sequences[sequence_idx][0];
+            for (size_t i = 1; i < sequences[sequence_idx].size(); ++i)
             {
                 const int current = sequences[sequence_idx][i];
-                const int diff = previous - current;
-                if (
+                const int diff = current - previous;
+                if (diff != 0)
+                {
+                    all_zero = false;
+                }
+                
+                sequences[sequence_idx + 1].push_back(diff);
                 previous = current;
             }
+            ++sequence_idx;
         }
-
         
+        for (sequence_idx = (int)sequences.size() - 1; sequence_idx >= 1; --sequence_idx)
+        {
+            const std::vector<int>& this_sequence = sequences[sequence_idx];
+            std::vector<int>& above_sequence = sequences[sequence_idx - 1];
+            const int increment = this_sequence[this_sequence.size() - 1];
+            above_sequence.push_back(above_sequence[above_sequence.size() - 1] + increment);
+        }
+        
+        end_total += sequences[0][sequences[0].size() - 1];
     }
-{
+    
+    std::cout << "End value total: " << end_total << std::endl;
+}
