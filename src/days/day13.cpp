6a4dd9eb186fb21530c037594287e2579cd51052
Stuart Brown <stuart.j.brown.90@gmail.com>
2023-12-14 00:14:57 +0000

    Part 2

--- src/days/day13.cpp
+++ src/days/day13.cpp
@@ -1,7 +1,7 @@
 #include "aoc_days.hpp"
 #include <vector>
 
-int scan_map(const std::vector<std::string>& map, bool vertical)
+int scan_map_direction(const std::vector<std::string>& map, bool vertical, int ignore_line = -1)
 {
     const int size = vertical ? (int)map.size() : (int)map[0].length();
     int mirror_pos = -1;
@@ -13,28 +13,53 @@ int scan_map(const std::vector<std::string>& map, bool vertical)
             if (vertical)
             {
                 if (map[scan_lhs].compare(map[scan_rhs]) != 0)
-                {
                     is_valid = false;
-                }
             }
             else
             {
                 for (int j = 0; j < map.size(); ++j)
                 {
                     if (map[j][scan_lhs] != map[j][scan_rhs])
-                    {
                         is_valid = false;
-                    }
                 }
             }
         }
-        if (is_valid)
+        if (is_valid && i + 1 != ignore_line)
         {
             mirror_pos = i + 1;
             break;
         }
     }
     return mirror_pos;
+}
+
+void scan_map(size_t& out_reflection_value, size_t& smudge_fix_reflection_value, const std::vector<std::string>& map)
+{
+    out_reflection_value = 0;
+    smudge_fix_reflection_value = 0;
+    
+    const int result_h = scan_map_direction(map, false);
+    const int result_v = scan_map_direction(map, true);
+    if (result_h != -1)
+        out_reflection_value = result_h;
+    else if (result_v != -1)
+        out_reflection_value = result_v * 100;
+    
+    for (size_t y = 0; y < map.size(); ++y)
+    {
+        for (size_t x = 0; x < map[0].length(); ++x)
+        {
+            std::vector<std::string> adjusted_map_copy = map;
+            adjusted_map_copy[y][x] = map[y][x] == '.' ? '#' : '.';
+            
+            const int adjusted_result_h = scan_map_direction(adjusted_map_copy, false, result_h);
+            const int adjusted_result_v = scan_map_direction(adjusted_map_copy, true, result_v);
+            if (adjusted_result_h != -1)
+                smudge_fix_reflection_value = adjusted_result_h;
+            if (adjusted_result_v != -1)
+                smudge_fix_reflection_value = adjusted_result_v * 100;
+        }
+    }
 }
 
 void aoc::day13()
@@ -51,20 +76,15 @@ void aoc::day13()
             maps.back().push_back(line);
     }
     
-    size_t reflection_summary = 0;
+    size_t reflection_summary = 0, smudge_fix_reflection_summary = 0;
+    size_t reflection_value = 0, smudge_fix_reflection_value = 0;
     for (const auto& map : maps)
     {
-        const int result_h = scan_map(map, false);
-        const int result_v = scan_map(map, true);
-        if (result_h != -1)
-        {
-            reflection_summary += result_h;
-        }
-        if (result_v != -1)
-        {
-            reflection_summary += result_v * 100;
-        }
+        scan_map(reflection_value, smudge_fix_reflection_value, map);
+        reflection_summary += reflection_value;
+        smudge_fix_reflection_summary += smudge_fix_reflection_value;
     }
     
     std::cout << "Reflection summary value: " << reflection_summary << std::endl;
+    std::cout << "Smudge-fixed reflection summary value: " << smudge_fix_reflection_summary << std::endl;
 }
